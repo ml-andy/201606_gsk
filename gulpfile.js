ff18be65e815a90be3f6d28ed852ff9b719fca95
AndyWang2015 <andy2472625@gmail.com>
2017-04-18 14:21:11 +0800

    new repo

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('js',function(){
 });
 gulp.task('connect', function() {
   $.connect.server({
-  	root: 'D:/yihsien/201606/20160608_gsk/gsk/andy/',
+  	root: './andy/',
   	livereload: true
   });
 });
